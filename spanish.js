dc328562530135b44308fd4f9e395f9cf1025a6e
Roland Rytz <roland.rytz@gmail.com>
2014-10-20 18:44:43 +0200

    Defined basic card deck structure and set up basic html site.

---
+++ spanish.js
@@ -0,0 +1,19 @@
+var spanishCards = {
+	 name: "Spanish"
+	,blocks: [
+		{
+			 name: "Chapter 1"
+			,cards: [
+				 ["foo", "bar"]
+				,["Lorem", "Ipsum"]
+			]
+		}
+		,{
+			 name: "Chapter 2"
+			,cards: [
+				 ["Hello", "Hola"]
+				,["Mir geht es gut", "Estoy bien."]
+			]
+		}
+	]
+}
